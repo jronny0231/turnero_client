0b5d30a7e15cca778e5acd1d50d183da9750c344
PC INVIS <jr.martinezvaldez@gmail.com>
2023-12-13 18:29:08 -0400

    set limited options and style controls

--- src/components/datatable/data.table.tsx
+++ src/components/datatable/data.table.tsx
@@ -3,15 +3,59 @@ import { DangerIcon, SpinnerIcon } from "../../lib/icons/helper.icons";
 import { SimpleTable, TableProps } from './lib/simple.table';
 //import { useReactTable } from "@tanstack/react-table"
 
-type customFieldStyle = 'rounded' | 'square'
+const CONTRAST = {
+    light: 300, dark: 800, hover: 200
+}
+
+enum customFieldSharpStyle {
+    rounded = 'rounded-xl',
+    square = 'rounded-none'
+}
+enum customFieldFillStyle {
+    filled = 'bg-$ text-#',
+    border = 'bg-transparent border-2 border-# text-#' 
+}
+
+const colors = {
+    prim: { light: `prim-${CONTRAST.light}`, dark: `prim-${CONTRAST.dark}`, hover: `prim-${CONTRAST.hover}` },
+    sec: { light: `sec-${CONTRAST.light}`, dark: `sec-${CONTRAST.dark}`, hover: `sec-${CONTRAST.hover}` },
+    blue: { light: `blue-${CONTRAST.light}`, dark: `blue-${CONTRAST.dark}`, hover: `blue-${CONTRAST.hover}` },
+    red: { light: `red-${CONTRAST.light}`, dark: `red-${CONTRAST.dark}`, hover: `red-${CONTRAST.hover}` },
+    yellow: { light: `yellow-${CONTRAST.light}`, dark: `yellow-${CONTRAST.dark}`, hover: `yellow-${CONTRAST.hover}` },
+    pink: { light: `pink-${CONTRAST.light}`, dark: `pink-${CONTRAST.dark}`, hover: `pink-${CONTRAST.hover}` },
+    slate: { light: `slate-${CONTRAST.light}`, dark: `slate-${CONTRAST.dark}`, hover: `slate-${CONTRAST.hover}` },
+    green: { light: `green-${CONTRAST.light}`, dark: `green-${CONTRAST.dark}`, hover: `green-${CONTRAST.hover}` },
+    zinc: { light: `zinc-${CONTRAST.light}`, dark: `zinc-${CONTRAST.dark}`, hover: `zinc-${CONTRAST.hover}` },
+    neutral: { light: `neutral-${CONTRAST.light}`, dark: `neutral-${CONTRAST.dark}`, hover: `neutral-${CONTRAST.hover}` },
+    stone: { light: `stone-${CONTRAST.light}`, dark: `stone-${CONTRAST.dark}`, hover: `stone-${CONTRAST.hover}` },
+    orange: { light: `orange-${CONTRAST.light}`, dark: `orange-${CONTRAST.dark}`, hover: `orange-${CONTRAST.hover}` },
+    amber: { light: `amber-${CONTRAST.light}`, dark: `amber-${CONTRAST.dark}`, hover: `amber-${CONTRAST.hover}` },
+    lime: { light: `lime-${CONTRAST.light}`, dark: `lime-${CONTRAST.dark}`, hover: `lime-${CONTRAST.hover}` },
+    emerald: { light: `emerald-${CONTRAST.light}`, dark: `emerald-${CONTRAST.dark}`, hover: `emerald-${CONTRAST.hover}` },
+    teal: { light: `teal-${CONTRAST.light}`, dark: `teal-${CONTRAST.dark}`, hover: `teal-${CONTRAST.hover}` },
+    sky: { light: `sky-${CONTRAST.light}`, dark: `sky-${CONTRAST.dark}`, hover: `sky-${CONTRAST.hover}` },
+    indigo: { light: `indigo-${CONTRAST.light}`, dark: `indigo-${CONTRAST.dark}`, hover: `indigo-${CONTRAST.hover}` },
+    violet: { light: `violet-${CONTRAST.light}`, dark: `violet-${CONTRAST.dark}`, hover: `violet-${CONTRAST.hover}` },
+    purple: { light: `purple-${CONTRAST.light}`, dark: `purple-${CONTRAST.dark}`, hover: `purple-${CONTRAST.hover}` },
+    fuchsia: { light: `fuchsia-${CONTRAST.light}`, dark: `fuchsia-${CONTRAST.dark}`, hover: `fuchsia-${CONTRAST.hover}` },
+    rose: { light: `rose-${CONTRAST.light}`, dark: `rose-${CONTRAST.dark}`, hover: `rose-${CONTRAST.hover}` },
+}
+
+type customFieldStyle = {
+    color: keyof typeof colors
+    sharp?: keyof typeof customFieldSharpStyle
+    fill?: keyof typeof customFieldFillStyle
+}
+
+type statusStyle = {
+    icon?: JSX.Element
+    style: customFieldStyle
+    value: string
+}
 
 type customField = {
     key: string
-    statusStyle: {
-        icon?: JSX.Element
-        style: string
-        value: string
-    }[]
+    statusStyle: statusStyle[]
     type?: customFieldStyle
     onClick?: (data: object) => void
 }
@@ -46,53 +90,63 @@ const buttons: TableProps['actions'] = {
     ]
 }
 
-type CustomFieldProps = {
-    style: string
-    value: string
-    icon?: JSX.Element
-    type?: customFieldStyle
-    action?: customField['onClick']
+interface CustomFieldProps extends statusStyle {
+    action?: {
+        cback: customField['onClick']
+        value: {[key: string]: unknown}
+    }
 }
 
-const CustomField = ({ type = 'rounded', ...rest }: CustomFieldProps) => {
+// CAMBIAR CLASES POR ESTILOS EN LA PROPIEDAD STYLE DEL ELEMENTO
+const calculateStyle = (data: customFieldStyle) => {
+    const sharp = customFieldSharpStyle[data.sharp ?? "rounded"]
+    const fill = customFieldFillStyle[data.fill ?? "filled"]
 
-    const style = `text-center w-fit p-2 flex flex-row flex-wrap item-center justify-center content-center h-full gap-1 ${type === 'rounded' ? 'rounded-xl' : ''} ${rest.style}`
+    const {dark, light, hover} = colors[data.color]
+
+    const hoverStyle = `hover:bg-${hover} transition-colors`
+
+    return `${sharp} ${fill.replace(/#/g, dark).replace(/\$/g, light)} ${hoverStyle}`
+}
+
+const CustomField = (props: CustomFieldProps) => {
+
+    const {action, icon, value} = props
+
+    const presetStyles = "text-center w-fit p-2 flex flex-row flex-wrap item-center justify-center content-center h-full gap-1";
+    const calculatedStyles = calculateStyle(props.style)
+    const style = `${presetStyles} ${calculatedStyles}`
+
     const Children = () => {
         return (
             <>
-                {rest.icon &&
+                {icon &&
                     <span>
-                        {rest.icon}
+                        {icon}
                     </span>
                 }
                 <span className='font-semibold'>
-                    {rest.value}
+                    {value}
                 </span>
             </>
         )
     }
 
-    // Buscar la manera de obtener los datos de la fila sin sobrecargar los metadatos
-    const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
-        const row = e.currentTarget.parentElement?.parentElement
-
-        const data = {
-            uuid: row?.getAttribute('data-uuid') ?? '',
-        }
-
-        if (rest.action && data) {
-            const rowData = {data}
-            rest.action(rowData)
+    // Returns the cell content to call back function
+    const handleClick = () => {
+        if (action && action.cback) {
+            const {cback, value} = action
+            cback(value)
         }
     }
 
     return (
         <>
-            {rest.action
-                ? <button onClick={handleClick} className={`${style} border-none`}>
+            {action
+                ? <button onClick={handleClick} className={style}>
                     <Children />
                 </button>
-                : <div className={`${style}`}>
+                : <div className={style}>
                     <Children />
                 </div>
             }
@@ -118,8 +172,7 @@ const performedRowData = async ({ row, customField }: { row: object, customField
                             value={customStatus.value}
                             style={customStatus.style}
                             icon={customStatus.icon}
-                            type={custom.type}
-                            action={custom.onClick}
+                            action={{cback: custom.onClick, value: {[key]:customStatus.value}}}
                         />
                         : newRow[key]
                 })
@@ -199,7 +252,7 @@ export const DataTable = (props: Props) => {
     }
 
     return (
-        <div className="flex flex-col w-full min-h-max shadow-prim-200 shadow-lg border-prim-400">
+        <div className="flex flex-col w-full min-h-max shadow-prim-${CONTRAST.hover} shadow-lg border-prim-400">
             {isLoading && <LoadingTag />}
             {hasErrors && <ErrorTag />}
             {!isLoading && !hasErrors && <>
